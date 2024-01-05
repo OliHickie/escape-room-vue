94a642647e649b23a88ced2a47af93c303a045cf
Oli Hickie <oli@thethinkingtraveller.com>
2024-01-05 15:58:56 +0000

    Create Button.vue

---
+++ escape/src/components/Base/Button.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+  <router-link class="px-6 py-2 bg-gray-100 border border-orange-700 text-orange-700 rounded font-semibold hover:bg-gray-300" @click="">
+    <slot />
+  </router-link>
+</template>
+
+
