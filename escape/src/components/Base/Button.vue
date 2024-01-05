30e76300d4234461ebc5d4ff4214c55f5ebfb313
Oli Hickie <oli@thethinkingtraveller.com>
2024-01-05 16:39:45 +0000

    Remove router-link from button

--- escape/src/components/Base/Button.vue
+++ escape/src/components/Base/Button.vue
@@ -4,9 +4,9 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <router-link class="px-6 py-2 bg-gray-100 border border-orange-700 text-orange-700 rounded font-semibold hover:bg-gray-300" @click="">
+  <button class="px-6 py-2 bg-gray-100 border border-orange-700 text-orange-700 rounded font-semibold hover:bg-gray-300" @click="">
     <slot />
-  </router-link>
+  </button>
 </template>
 
 
