87fe55f0cbac0485d31b7bc5eff020e6228dcadc
Oli Hickie <oli@thethinkingtraveller.com>
2024-01-05 15:58:53 +0000

    Create Header.vue

---
+++ escape/src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="text-center py-4">
+    <h1 class="text-4xl font-lora uppercase font-semibold text-gray-100">Escape GWR HQ</h1>
+  </div>
+</template>
