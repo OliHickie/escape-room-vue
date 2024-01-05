7a22a9e939341fd5600cf659a6d6475787cf3a0f
Oli Hickie <oli@thethinkingtraveller.com>
2024-01-05 15:02:31 +0000

    init commit

---
+++ escape/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+import { fontFamily as _fontFamily } from 'tailwindcss/defaultTheme';
+
+export const content = [
+  "./index.html",
+  "./src/**/*.{js, jsx}",
+];
+export const theme = { 
+  extend: {
+    fontFamily: {
+      lora: ['"Lora"', ..._fontFamily.sans],
+      alegreya: ['"Alegreya"', ..._fontFamily.serif]
+    }
+  },
+};
+export const plugins = [];
