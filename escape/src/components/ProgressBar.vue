c0d6d4915fa5d1190ae148b508bb836f92b884a4
Oli Hickie <oli@thethinkingtraveller.com>
2024-01-05 16:39:51 +0000

    Create ProgressBar.vue

---
+++ escape/src/components/ProgressBar.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useRoute} from 'vue-router';
+import { computed } from 'vue';
+
+const route = useRoute();
+
+const progress = computed(() => {
+  const width = route.meta.question * 20
+  console.log(width)
+  return {
+    width: `${width}%`
+  }
+})
+
+</script>
+
+<template>
+  <div class="border border-orange-700 w-full h-8">
+    <div class="bg-orange-300 h-full" :style="progress"></div>
+  </div>
+</template>
