c91f4fa928a1680330cd39e526191cb78273acb5
Oli Hickie <oli@thethinkingtraveller.com>
2024-01-05 16:40:15 +0000

    Update GameArea to include progressbar

--- escape/src/layouts/GameArea.vue
+++ escape/src/layouts/GameArea.vue
@@ -1,9 +1,14 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router';
+import ProgressBar from '../components/ProgressBar.vue'
+
+const route = useRoute();
+
 </script>
 
 <template>
 <div class="bg-gray-100 w-3/5 mx-auto p-8">
+  <ProgressBar v-if="route.meta.question" />
   <RouterView />
 </div>
 </template>
