b8dc0d0ce84f7bb8abc81141585cb73ccee3d312
Oli Hickie <oli@thethinkingtraveller.com>
2024-01-06 13:19:39 +0000

    Create GamePlay.vue

---
+++ escape/src/views/GamePlay.vue
@@ -0,0 +1,13 @@
+<script setup>
+import GreyCircle from '@/components/Base/GreyCircle.vue';
+import FootballRiddle from '../components/FootballRiddle.vue';
+
+</script>
+
+<template>
+  <div class="relative flex justify-center items-center">
+    <img src="../assets/images/office-space.jpg" alt="" class="w-full absolute top-0 left-0">
+    <GreyCircle class="h-24 top-48 right-10 " />
+    <FootballRiddle class="relative " />
+  </div>
+</template>
