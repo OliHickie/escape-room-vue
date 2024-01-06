0e018c16157f77ecffb4030a71c7c33daebba357
Oli Hickie <oli@thethinkingtraveller.com>
2024-01-06 15:38:57 +0000

    Add magnify glass

--- escape/src/components/Base/GreyCircle.vue
+++ escape/src/components/Base/GreyCircle.vue
@@ -1,3 +1,9 @@
+<script setup>
+  import MagnifyGlass from './Icons/MagnifyGlass.vue';
+</script>
+
 <template>
-  <div class=" rounded-full bg-black/50 aspect-square absolute cursor-pointer" />
+  <div class="flex justify-center items-center rounded-full bg-black/50 aspect-square absolute cursor-pointer">
+    <MagnifyGlass class="h-12 w-12" />
+  </div>
 </template>
