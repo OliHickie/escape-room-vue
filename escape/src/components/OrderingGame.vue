57450cdb60522eedb69bb6dae3610a39d606c880
Oli Hickie <oli@thethinkingtraveller.com>
2024-01-07 11:37:49 +0000

    Update ordering game for logic, results page, animations

--- escape/src/components/OrderingGame.vue
+++ escape/src/components/OrderingGame.vue
@@ -1,6 +1,8 @@
 <script setup>
-import { onMounted, ref, defineAsyncComponent } from 'vue';
+import { onMounted, ref, computed} from 'vue';
 import Arrow from './Base/Icons/Arrow.vue';
+import gameData from '../definitions/gameInfo';
+import Button from './Base/Button.vue';
 
 const imagesData = ref([]);
 const answerData = ref([]);
@@ -9,26 +11,35 @@ onMounted(async () => {
   imagesData.value = [
     { 
       id: 1,
-      alt: 'Cow',
+      name: 'Largest ever cow',
       src: (await import('../assets/images/tallest-cow.webp')).default,
       shake: false,
+      url: 'https://www.guinnessworldrecords.com/world-records/70851-largest-cow-ever',
+      height: 190,
     },
     { 
       id: 2,
-      alt: 'Dog',
+      name: 'Tallest ever dog',
       src: (await import('../assets/images/tallest-dog.webp')).default,
       shake: false,
+      url: 'https://www.guinnessworldrecords.com/world-records/tallest-dog-ever',
+      height: 111,
     },
     { 
       id: 3,
-      alt: 'Man',
+      name: 'Tallest man ever',
       src: (await import('../assets/images/tallest-man.webp')).default,
-      shake: false, 
+      shake: false,
+      url: 'https://www.guinnessworldrecords.com/world-records/tallest-man-ever',
+      height: 272, 
     },
-    { id: 4,
-      alt: 'Waffles',
+    { 
+      id: 4,
+      name: 'Tallest stack of waffles',
       src: (await import('../assets/images/tallest-waffles.webp')).default,
-      shake: false, 
+      shake: false,
+      url: 'https://www.guinnessworldrecords.com/world-records/437476-tallest-stack-of-waffles',
+      height: 91.5
     },
   ];
 });
@@ -50,14 +61,16 @@ const removeFromAnswer = (image) => {
 
 const checkAnswers = () => {
   const order = [4, 2, 1, 3];
-  // If the 
   for (let i = 0; i < answerData.value.length; i++) {
+    // If the order is incorrect
     if (answerData.value[i].id !== order[i]) {
       clearAnswers();
       return false;
+    } else {
+      // If the order is correct
+      gameData.ordering.completed = true;
     }
   }
-  console.log('CORRECT')
 }
 
 const clearAnswers = () => {
@@ -73,45 +86,70 @@ const clearAnswers = () => {
 </script>
 
 <template>
-  <div class="h-full w-screen bg-black text-center pt-12 text-gray-100">
-    <h2 class="text-2xl font-lora">Can you solve this problem?</h2>
-    <p class="mt-4">
-      Somebody has mixed up this collection of 'World's tallest' records, can you put them back in height order, from shortest to tallest?
-    </p>
-    <div class="mt-12 flex justify-center gap-1">
-      <div v-for="image in imagesData" :key="image.id" class="relative" @click="addToAnswer(image)">
-        <img :src="image.src" :alt="image.alt" class="w-28 h-40 object-cover" >
-        <div :class="['absolute bottom-0 left-0 right-0 bg-black/70 h-full flex items-center justify-center hover:opacity-100', answerData.includes(image) ? 'opacity-100' : 'opacity-0' ]">
-          <span v-if="!answerData.includes(image)" class="font-semibold text-4xl">+</span>
+  <div class="min-h-full w-screen bg-black text-center pt-12 text-gray-100">
+    <div v-if="!gameData.ordering.completed">
+      <h2 class="text-2xl font-lora">Can you solve this problem?</h2>
+      <p class="mt-4">
+        Somebody has mixed up this collection of 'World's tallest' records, can you put them back in height order, from shortest to tallest?
+      </p>
+      <div class="mt-12 flex justify-center gap-1">
+        <div v-for="image in imagesData" :key="image.id" class="relative cursor-pointer" @click="addToAnswer(image)">
+          <img :src="image.src" :alt="image.name" class="w-28 h-40 object-cover" >
+          <div :class="['absolute bottom-0 left-0 right-0 bg-black/70 h-full flex items-center justify-center hover:opacity-100', answerData.includes(image) ? 'opacity-100' : 'opacity-0' ]">
+            <span v-if="!answerData.includes(image)" class="font-semibold text-4xl">+</span>
+          </div>
+        </div>
+      </div>
+      <div>Click on the image to add it to the order</div>
+      <div class="mt-12 flex justify-between w-[768px] mx-auto">
+        <span>Shortest</span>
+        <Arrow />
+        <span>Tallest</span>
+      </div>
+      <div class="flex justify-center gap-1">
+        <div v-for="image in answerData" :key="image.id" class="relative cursor-pointer" @click="removeFromAnswer(image)">
+          <img :src="image.src" :alt="image.name" class="w-48 h-60 object-cover" :class="{ shake: image.shake }">
+          <div class="absolute bottom-0 left-0 right-0 bg-black/70 h-full flex justify-end opacity-0 hover:opacity-100">
+            <span class="font-semibold text-4xl mr-2">x</span>
+          </div>
+        </div>
+        <div v-for="index in (4 - answerData.length)" :key="index">
+          <div class="w-48 h-60 border border-orange-300" />
         </div>
       </div>
     </div>
-    <div>Click on the image to add it to the order</div>
-    <div class="mt-12 flex justify-between w-[768px] mx-auto">
-      <span>Shortest</span>
-      <Arrow />
-      <span>Tallest</span>
-    </div>
-    <div class="flex justify-center gap-1">
-    <div v-for="image in answerData" :key="image.id" class="relative" @click="removeFromAnswer(image)">
-      <img :src="image.src" :alt="image.alt" class="w-48 h-60 object-cover" :class="{ shake: image.shake }">
-      <div class="absolute bottom-0 left-0 right-0 bg-black/70 h-full flex items-center justify-center opacity-0 hover:opacity-100">
-        <span class="font-semibold text-4xl">-</span>
+    <div v-if="gameData.ordering.completed" class="space-y-6">
+      <h2 class="text-4xl">CONGRATULATIONS</h2>
+      <p>You have managed to succesfully put our World's Tallest Records back in height order!</p>
+      <div class="flex gap-2 justify-center">
+        <a
+          v-for="image in answerData" 
+          :href="image.url" 
+          target="_blank"  
+          :key="image.id" 
+          class="relative"
+        >
+          <img :src="image.src" :alt="image.name" class="w-48 h-60 object-cover" :class="{ shake: image.shake }">
+          <div class="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-gray-900/90 h-full flex items-end justify-center">
+            <div>
+              <p class="font-semibold text-lg">{{ image.name }}</p>
+              <p>Height: {{ image.height }}cm</p>
+              <a :href="image.url">Read more</a>
+            </div>
+          </div>
+        </a>
       </div>
+      <Button @click="$emit('close')">Continue</Button>
     </div>
-    <div v-for="index in (4 - answerData.length)" :key="index">
-      <div class="w-48 h-60 border border-orange-300" />
-    </div>
-  </div>
   </div>
 </template>
 
 <style>
   @keyframes shake {
     0% { transform: translateX(0); }
-    25% { transform: translateX(-5px) rotate(0.5deg); }
-    50% { transform: translateX(5px) rotate(-0.5deg); }
-    75% { transform: translateX(-5px) rotate(0.5deg); }
+    25% { transform: translateX(-10px) rotate(1deg); }
+    50% { transform: translateX(10px) rotate(-1deg); }
+    75% { transform: translateX(-10px) rotate(1deg); }
     100% { transform: translateX(0); }
   }
 
