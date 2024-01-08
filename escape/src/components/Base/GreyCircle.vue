98a50c3ba350402e05086fc3918df1432b05f69c
Oli Hickie <oli@thethinkingtraveller.com>
2024-01-08 19:19:53 +0000

    Add green check to circles

--- escape/src/components/Base/GreyCircle.vue
+++ escape/src/components/Base/GreyCircle.vue
@@ -1,9 +1,16 @@
 <script setup>
-  import MagnifyGlass from './Icons/MagnifyGlass.vue';
+import MagnifyGlass from './Icons/MagnifyGlass.vue';
+import CheckIcon from './Icons/CheckIcon.vue';
+
+defineProps({
+  completed: { type: Boolean, default: false  },
+})
+
 </script>
 
 <template>
   <div class="flex justify-center items-center rounded-full bg-black/50 aspect-square cursor-pointer">
-    <MagnifyGlass class="w-6 md:w-12" />
+    <MagnifyGlass v-if="!completed" class="w-6 md:w-12" />
+    <CheckIcon v-else class="w-6 md:w-12" />
   </div>
 </template>
